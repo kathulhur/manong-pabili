72c7d025dc40d21cbe0888fbad2ea37bb6646967
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2023-08-29 15:17:02 +0800

    Update usePusher hook to include the channel

--- web/src/hooks/usePusher.tsx
+++ web/src/hooks/usePusher.tsx
@@ -1,15 +1,18 @@
-import Pusher from "pusher-js"
+import Pusher, { Channel } from "pusher-js"
 import { useEffect, useState } from "react"
 
 
 
 export default function usePusher() {
   const [pusher, setPusher] = useState<Pusher>(null)
+  const [channel, setChannel] = useState<Channel>(null)
   useEffect(() => {
     const pusher = new Pusher(process.env.PUSHER_APP_KEY, {
         cluster: process.env.PUSHER_APP_CLUSTER,
     })
+    const channel = pusher.subscribe(process.env.PUSHER_CHANNEL)
     setPusher(pusher)
+    setChannel(channel)
     console.log('pusher initialized')
     return () => {
         console.log('pusher disconnecting...')
@@ -18,5 +21,5 @@ export default function usePusher() {
 
   }, [])
 
-  return pusher
+  return [pusher, channel]
 }
