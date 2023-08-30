d0fa2946ac868e25930e4f6d4a6a43c2b95b506e
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2023-08-30 09:49:37 +0800

    Implement role based authentication

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -14,20 +14,23 @@ import { useAuth } from './auth'
 const Routes = () => {
   return (
     <Router useAuth={useAuth}>
+      <Route path="/forbidden" page={ForbiddenPage} name="forbidden" />
       <Route path="/" page={IndexPage} name="index" />
       <Route path="/login" page={LoginPage} name="login" />
       <Route path="/signup" page={SignupPage} name="signup" />
       <Route path="/forgot-password" page={ForgotPasswordPage} name="forgotPassword" />
       <Route path="/reset-password" page={ResetPasswordPage} name="resetPassword" />
-        <Route path="/map" page={MapPage} name="map" />
-      <Private unauthenticated="login">
+      <Route path="/map" page={MapPage} name="map" />
+      <Private unauthenticated="forbidden" roles={"VENDOR"}>
         <Route path="/vendor" page={HomePage} name="home" />
         <Route path="/vendor/products" page={ProductsPage} name="products" />
         <Route path="/vendor/account" page={VendorAccountPage} name="vendorAccount" />
+        <Route notfound page={NotFoundPage} />
+      </Private>
+      <Private unauthenticated='forbidden' roles={"ADMIN"}>
         <Route path="/admin" page={AdminPage} name="admin" />
         <Route path="/admin/signup" page={AdminSignupPage} name="adminSignup" />
         <Route path="/admin/login" page={AdminLoginPage} name="adminLogin" />
-        <Route notfound page={NotFoundPage} />
       </Private>
     </Router>
   )
---
+++ web/src/pages/ForbiddenPage/ForbiddenPage.tsx
@@ -0,0 +1,37 @@
+import { Link, navigate, routes } from "@redwoodjs/router";
+import { MetaTags } from "@redwoodjs/web";
+import { useAuth } from "src/auth";
+
+const ForbiddenPage = () => {
+  const { isAuthenticated, loading, currentUser } = useAuth();
+
+  if (loading) {
+    return null
+  }
+
+  if (isAuthenticated) {
+    if (currentUser.roles.includes('ADMIN')) {
+      navigate(routes.admin())
+    } else if(currentUser.roles.includes('VENDOR')) {
+      navigate(routes.home())
+      return null
+
+    } else {
+      navigate(routes.index())
+    }
+  }
+
+  return (
+    <>
+      <MetaTags title="Forbidden" description="Forbidden page" />
+
+      <h1>You don't have permission to visit this page</h1>
+      <p>
+        <Link to={routes.index()}>Go home</Link>
+      </p>
+
+    </>
+  );
+};
+
+export default ForbiddenPage;
