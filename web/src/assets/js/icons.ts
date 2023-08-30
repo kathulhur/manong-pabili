a5600f1968897787aadaaf9b8215ef60ac4e6965
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2023-08-31 07:15:28 +0800

    Add initial icon sets

---
+++ web/src/assets/js/icons.ts
@@ -0,0 +1,27 @@
+import juices from '../images/juices.png'
+import bbq_grill from '../images/bbq-grill.png'
+import coconut from '../images/coconut.png'
+import kiosk from '../images/kiosk.png'
+import meatball from '../images/meatball.png'
+
+
+const images = {
+  juices,
+  bbq_grill,
+  coconut,
+  kiosk,
+  meatball
+}
+
+type IconKeys = keyof typeof images
+const icons: Record<IconKeys, HTMLImageElement> = {} as Record<IconKeys, HTMLImageElement>;
+
+for (const [key, value] of Object.entries(images)) {
+  let image = document.createElement('img')
+  image.src = value
+  image.className = 'h-8 w-8'
+
+  icons[key] = image
+}
+
+export default icons;
