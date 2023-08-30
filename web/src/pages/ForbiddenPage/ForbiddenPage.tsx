6f50793082c9507dcf7cf7e62acb81304053783d
JosephCrisostomo <joseph.crisostomo@dashlabs.ai>
2023-08-30 22:11:52 +0800

    Fix redirection errors when navigating to forbidden pages

--- web/src/pages/ForbiddenPage/ForbiddenPage.tsx
+++ web/src/pages/ForbiddenPage/ForbiddenPage.tsx
@@ -1,25 +1,30 @@
 import { Link, navigate, routes } from "@redwoodjs/router";
 import { MetaTags } from "@redwoodjs/web";
+import { useEffect } from "react";
 import { useAuth } from "src/auth";
 
 const ForbiddenPage = () => {
   const { isAuthenticated, loading, currentUser } = useAuth();
 
+  useEffect(() => {
+    if (isAuthenticated && currentUser) {
+      if (currentUser.roles.includes('ADMIN')) {
+        navigate(routes.admin())
+      } else if(currentUser.roles.includes('VENDOR')) {
+        navigate(routes.home())
+        return null
+      } else {
+        navigate(routes.index())
+      }
+    }
+
+  }, [isAuthenticated, currentUser])
+
+
   if (loading) {
     return null
   }
 
-  if (isAuthenticated && currentUser) {
-    if (currentUser.roles.includes('ADMIN')) {
-      navigate(routes.admin())
-    } else if(currentUser.roles.includes('VENDOR')) {
-      navigate(routes.home())
-      return null
-
-    } else {
-      navigate(routes.index())
-    }
-  }
 
   return (
     <>
