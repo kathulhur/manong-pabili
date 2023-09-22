70c62d04a118053b5a44f73051929e1720c84bef
Greg <gdsuaperoii@iskolarngbayan.pup.edu.ph>
2023-09-22 16:27:08 +0800

    change style of toggle

--- web/src/components/Vendor/VendorHomepageCell/VendorHomepageCell.tsx
+++ web/src/components/Vendor/VendorHomepageCell/VendorHomepageCell.tsx
@@ -494,17 +494,17 @@ export const Success = ({
                             : showLocationButtonHandler
                     }
                     className={`${
-                        isLocationShown ? 'bg-green-600' : 'bg-green-400'
+                        isLocationShown ? 'bg-green-600' : 'bg-gray-200'
                     }
-              relative inline-flex h-[28px] w-[56px] shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus-visible:ring-2  focus-visible:ring-white focus-visible:ring-opacity-75`}
+                    relative inline-flex h-6 w-11 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2`}
                 >
                     <span className="sr-only">Show location</span>
                     <span
                         aria-hidden="true"
                         className={`${
-                            isLocationShown ? 'translate-x-7' : 'translate-x-0'
+                            isLocationShown ? 'translate-x-5' : 'translate-x-0'
                         }
-                  pointer-events-none inline-block h-[24px] w-[24px] transform rounded-full bg-white shadow-lg ring-0 transition duration-200 ease-in-out`}
+                        pointer-events-none inline-block h-5 w-5 transform rounded-full bg-white shadow ring-0 transition duration-200 ease-in-out`}
                     />
                 </Switch>
             </div>
