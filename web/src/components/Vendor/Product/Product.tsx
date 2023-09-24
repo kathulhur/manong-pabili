13e62ac1dc6f1f1dee96e7765688d6e216dbbbfc
Greg <gdsuaperoii@iskolarngbayan.pup.edu.ph>
2023-09-24 11:11:32 +0800

    change width of menu items

--- web/src/components/Vendor/Product/Product.tsx
+++ web/src/components/Vendor/Product/Product.tsx
@@ -156,7 +156,7 @@ const Product = ({
                         leaveFrom="transform opacity-100 scale-100"
                         leaveTo="transform opacity-0 scale-95"
                     >
-                        <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
+                        <Menu.Items className="absolute right-0 z-10 mt-2 w-44 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
                             {[
                                 {
                                     label: product.availability
